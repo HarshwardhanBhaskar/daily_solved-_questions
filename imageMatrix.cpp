8092cc7bcbcc124484cf881f25848b54bfdc2f1b
Harshwardhan Baskar <hwbhaskar60147@gmail.com>
2026-02-08 18:24:21 +0530

    Image Matrix solved uisng simple rule roatation od 2D matrix

---
+++ imageMatrix.cpp
@@ -0,0 +1,36 @@
+// simply transpose type 
+#include<bits/stdc++.h>
+using namespace std;    
+class Solution{
+    public:
+    void imagematrix(vector<vector<int>>&matrix){
+        int n=matrix.size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        for(int i=0;i<n;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>>matrix(n,vector<int>(n));
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>matrix[i][j];
+        }
+    }
+    Solution s;
+    s.imagematrix(matrix);
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
